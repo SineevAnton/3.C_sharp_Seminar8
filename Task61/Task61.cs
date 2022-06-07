831aeac236a5efde231524bc68d6ae4c04ff5952
Anton Sineev <sineevanton@mail.ru>
2022-06-07 13:30:43 +0300

    Task61 complete

---
+++ Task61/Task61.cs
@@ -0,0 +1,39 @@
+﻿// Print the first N lines of Pascal's triangle 
+// in the form of an isosceles triangle
+
+Console.Write("Enter number of lines to show: ");
+int linesNum = int.Parse(Console.ReadLine());
+
+// Method to find factorial of number.
+// Will be used to get an element of tha Pascal triangle
+// P.s. we use float type cause with int there will be
+// mistakes while counting big numbers
+float Factorial(int n)
+{
+    float result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+// Method to print Pascal triangle into the console
+void ShowPascalTriangle(int linesCount)
+{
+    for (int i = 0; i < linesNum; i++)
+    {
+        for (int j = 0; j <= (linesNum - i); j++)
+        {
+            Console.Write("   ");
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            Console.Write("     ");
+            Console.Write(Factorial(i) / (Factorial(k) * Factorial(i - k)));
+        }
+        Console.WriteLine("\n");
+    }
+}
+
+ShowPascalTriangle(linesNum);
